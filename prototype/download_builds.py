c34bb5c6942d296ae8af0f6a5076a0ceb4b6244d
Satish Goda <satishgoda@live.com>
2018-05-07 07:57:40 +0800

    Update download_builds.py

--- prototype/download_builds.py
+++ prototype/download_builds.py
@@ -1,10 +1,12 @@
 from HTMLParser import HTMLParser
+
 import urllib2
+
 from bs4 import BeautifulSoup
 
 download_base = "https://builder.blender.org"
 
-links_page = download_base + '/' + "download"
+links_page = "https://builder.blender.org/download"
 
 page = urllib2.urlopen(links_page)
 
@@ -14,13 +16,10 @@ tables = soup.findAll('table', attrs={'class': 'table table-striped table-hover 
 
 for table in tables:
     anchors = table.findAll('a')
-    
     for anchor in anchors:
-        print anchor.attrs
-        
-        donwload_link = download_base+anchor.attrs['href']
-        
-        dl = urllib2.urlopen(donwload_link)
-        
-        with open(os.path.basename(anchor.attrs['href']), 'wb') as output:
+        print "Downloading", anchor.attrs['href']
+        download_link = download_base+anchor.attrs['href']
+        dl = urllib2.urlopen(download_link)
+        dest_path = os.path.basename(anchor.attrs['href'])
+        with open(dest_path, 'wb') as output:
             output.write(dl.read())
