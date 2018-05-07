d0133bdc95a864b5772f0d244db040d7f8dd9da8
Satish Goda <satishgoda@live.com>
2018-05-07 12:35:22 +0800

    Update download_builds.py

--- prototype/download_builds.py
+++ prototype/download_builds.py
@@ -1,25 +1,60 @@
-from HTMLParser import HTMLParser
-
+import pathlib2
 import urllib2
-
+from HTMLParser import HTMLParser
 from bs4 import BeautifulSoup
 
-download_base = "https://builder.blender.org"
 
-links_page = "https://builder.blender.org/download"
+class DownloadSource(object):
+    def __init__(self, download_path):
+        self.download_path = download_path
+        self.page = None
+    
+    def prepare(self):
+        self.page = urllib2.urlopen(self.download_path)
+    
+    def get_files(self):
+        files_to_download = []
 
-page = urllib2.urlopen(links_page)
+        soup = BeautifulSoup(self.page, 'html.parser')
+        tables = soup.findAll('table', attrs={'class': 'table table-striped table-hover box'})
+        
+        for table in tables:
+            anchors = table.findAll('a')
+            for anchor in anchors:
+                download_link = self.download_path + '/' + anchor.text
+                files_to_download.append((anchor.text, download_link))
+        
+        return files_to_download
 
-soup = BeautifulSoup(page, 'html.parser')
+    @classmethod
+    def downloadURL(cls, url, destination_path):
+        opened_url = urllib2.urlopen(url)
+        print "Downloading to {0}".format(destination_path)
 
-tables = soup.findAll('table', attrs={'class': 'table table-striped table-hover box'})
+        with open(str(destination_path), 'wb') as output:
+            output.write(opened_url.read())
 
-for table in tables:
-    anchors = table.findAll('a')
-    for anchor in anchors:
-        print "Downloading", anchor.attrs['href']
-        download_link = download_base+anchor.attrs['href']
-        dl = urllib2.urlopen(download_link)
-        dest_path = os.path.basename(anchor.attrs['href'])
-        with open(dest_path, 'wb') as output:
-            output.write(dl.read())
+
+class DownloadDestination(object):
+    def __init__(self, download_path):
+        self.download_path = pathlib2.Path(download_path).expanduser()
+
+    def prepare(self):
+        self._create_download_path()
+
+    def _create_download_path(self):
+        if not self.download_path.exists():
+            self.download_path.mkdir()
+
+
+if __name__ == '__main__':
+    download_source = DownloadSource("https://builder.blender.org/download")
+    download_source.prepare()
+    files = download_source.get_files()
+    
+    download_destination = DownloadDestination('~/Downloads/Blender/BlenderToolbox/files')
+    download_destination.prepare()
+    
+    for filename, url in files:
+        destination_path = download_destination.download_path / filename
+        download_source.downloadURL(url, destination_path)
